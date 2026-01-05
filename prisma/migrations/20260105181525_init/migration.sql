9660cd0a27f3a232eed5769638e37ee0fd3a30fa
zipthezap <fontaineolivier456@gmail.com>
2026-01-05 13:34:16 -0500

    wip

--- prisma/migrations/20260105181525_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "EmailPreference" ADD COLUMN     "muteEventCancellations" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteEventCreated" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteEventInvites" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteEventReminders" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteEventUpdates" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteGroupInvites" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteGroupRequests" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "muteNearbyGroups" BOOLEAN NOT NULL DEFAULT false;
